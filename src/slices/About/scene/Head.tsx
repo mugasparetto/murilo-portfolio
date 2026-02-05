e4e7159dffe908fa0c8595bbd6083b03579a4e95
Murilo Gasparetto <mu.gasparetto@gmail.com>
2026-02-05 12:52:09 +0000

    added circles to Head

--- src/slices/About/scene/Head.tsx
+++ src/slices/About/scene/Head.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
 
 export default function Head() {
@@ -16,13 +17,44 @@ export default function Head() {
   return (
     <>
       <sprite position={[-380, -800, 2400]} scale={scale}>
-        <spriteMaterial map={bottom} transparent depthWrite={false} />
+        <spriteMaterial map={top} transparent depthWrite={false} />
       </sprite>
+      <mesh
+        position={[-369, -735, 2400]}
+        rotation={[Math.PI / 2 - 0.02, 0.02, 0]}
+        renderOrder={999}
+      >
+        <circleGeometry args={[122, 48]} />
+        <meshBasicMaterial
+          color={"hotpink"}
+          side={THREE.DoubleSide}
+          transparent
+          opacity={1}
+          depthTest={false}
+          depthWrite={false}
+        />
+      </mesh>
       <sprite position={[-380, -800, 2400]} scale={scale}>
         <spriteMaterial map={middle} transparent depthWrite={false} />
       </sprite>
-      <sprite position={[-380, -800, 2400]} scale={scale}>
-        <spriteMaterial map={top} transparent depthWrite={false} />
+      <mesh
+        position={[-372, -860, 2400]}
+        rotation={[Math.PI / 2 + 0.03, -0.03, 0]}
+        renderOrder={999}
+      >
+        <circleGeometry args={[122, 48]} />
+        <meshBasicMaterial
+          color={"hotpink"}
+          side={THREE.DoubleSide}
+          transparent
+          opacity={1}
+          depthTest={false}
+          depthWrite={false}
+        />
+      </mesh>
+
+      <sprite position={[-380, -800, 2400]} scale={scale} renderOrder={0}>
+        <spriteMaterial map={bottom} transparent depthWrite={false} />
       </sprite>
     </>
   );
